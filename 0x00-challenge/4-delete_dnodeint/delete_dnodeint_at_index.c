1d4cff6b143742915d6e5d3537bcd562e2a2279a
ibrahemesam <iimosa7777@gmail.com>
2024-02-17 08:57:11 +0200

    blabla bla

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -44,7 +44,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	else
 	{
 		(*head)->prev->next = (*head)->next;
-		/* (*head)->prev->prev = (*head)->prev; */
 		if ((*head)->next)
 			(*head)->next->prev = (*head)->prev;
 		free(*head);
